93aa5cbf57b92e38f9e395019c5e659160041710
Quiatus <Quiatus@gmail.com>
2024-06-07 14:52:15 +0200

    add map api

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+//const mapDiv = document.querySelector('#map')
+
+navigator.geolocation.getCurrentPosition((position) => {
+    const {latitude} = position.coords
+    const {longitude} = position.coords
+
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView(coords, 15);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup();
+
+},() => alert('Error while retrieving location! Check if location permisson is set to Allow.'))
